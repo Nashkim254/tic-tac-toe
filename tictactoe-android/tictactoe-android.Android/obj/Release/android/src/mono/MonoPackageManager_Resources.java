131d83a3be111c573b8149100db1f78f6e11a119
programming6131 <kimutainashon@gmail.com>
2022-03-29 21:36:11 +0300

    initial commit

---
+++ tictactoe-android/tictactoe-android.Android/obj/Release/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,46 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "tictactoe-android.Android.dll" comes first in this list. */
+		"tictactoe-android.Android.dll",
+		"FormsViewGroup.dll",
+		"Microsoft.WindowsAzure.Mobile.dll",
+		"Microsoft.WindowsAzure.Mobile.Ext.dll",
+		"Microsoft.WindowsAzure.Mobile.SQLiteStore.dll",
+		"Newtonsoft.Json.dll",
+		"SQLitePCLRaw.batteries_green.dll",
+		"SQLitePCLRaw.core.dll",
+		"SQLitePCLRaw.lib.e_sqlite3.dll",
+		"SQLitePCLRaw.provider.e_sqlite3.dll",
+		"System.Net.Http.Extensions.dll",
+		"System.Net.Http.Primitives.dll",
+		"tictactoe-android.dll",
+		"Xamarin.AndroidX.Activity.dll",
+		"Xamarin.AndroidX.AppCompat.AppCompatResources.dll",
+		"Xamarin.AndroidX.AppCompat.dll",
+		"Xamarin.AndroidX.CardView.dll",
+		"Xamarin.AndroidX.CoordinatorLayout.dll",
+		"Xamarin.AndroidX.Core.dll",
+		"Xamarin.AndroidX.CustomView.dll",
+		"Xamarin.AndroidX.DrawerLayout.dll",
+		"Xamarin.AndroidX.Fragment.dll",
+		"Xamarin.AndroidX.Legacy.Support.Core.UI.dll",
+		"Xamarin.AndroidX.Lifecycle.Common.dll",
+		"Xamarin.AndroidX.Lifecycle.LiveData.Core.dll",
+		"Xamarin.AndroidX.Lifecycle.ViewModel.dll",
+		"Xamarin.AndroidX.Loader.dll",
+		"Xamarin.AndroidX.RecyclerView.dll",
+		"Xamarin.AndroidX.SavedState.dll",
+		"Xamarin.AndroidX.SwipeRefreshLayout.dll",
+		"Xamarin.AndroidX.ViewPager.dll",
+		"Xamarin.Essentials.dll",
+		"Xamarin.Forms.Core.dll",
+		"Xamarin.Forms.Platform.Android.dll",
+		"Xamarin.Forms.Platform.dll",
+		"Xamarin.Forms.Xaml.dll",
+		"Xamarin.Google.Android.Material.dll",
+		"Xamarin.Google.Guava.ListenableFuture.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
